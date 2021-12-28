389f83ff7f78d5c6863deb513cae89cbb3b7729e
seblemeur <sebastienlemeur44nantes@gmail.com>
2021-12-28 19:43:32 +0100

    Add new example : galaxy

---
+++ pages/examples/galaxy.tsx
@@ -0,0 +1,170 @@
+import type { NextPage } from "next";
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CanvasContainer from "../../components/canvas-container";
+
+const Galaxy: NextPage = () => {
+  // Canvas
+  const canvas = useRef<HTMLDivElement>(null);
+
+  // Sizes
+  const sizes = {
+    width: 1000,
+    height: 600,
+  };
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.x = 4;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+
+  // Ambient light
+  const ambientLight = new THREE.AmbientLight("#ffffff", 1);
+  scene.add(ambientLight);
+
+  /**
+   * Galaxy
+   */
+  const parameters = {
+    count: 100000,
+    size: 0.01,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.2,
+    randomnessPower: 3,
+    insideColor: "#ff6030",
+    outsideColor: "#1b3984",
+  };
+
+  let geometry: any = null;
+  let material: any = null;
+  let points: any = null;
+
+  const generateGalaxy = () => {
+    /**
+     * Destroy old galaxy
+     */
+    if (points !== null) {
+      geometry.dispose();
+      material.dispose();
+      scene.remove(points);
+    }
+
+    geometry = new THREE.BufferGeometry();
+
+    const positions = new Float32Array(parameters.count * 3);
+    const colors = new Float32Array(parameters.count * 3);
+
+    const colorInside = new THREE.Color(parameters.insideColor);
+    const colorOutside = new THREE.Color(parameters.outsideColor);
+
+    for (let i = 0; i < parameters.count; i++) {
+      const i3 = i * 3;
+
+      // position
+      const radius = Math.random() * parameters.radius;
+      const spinAngle = radius * parameters.spin;
+      const branchAngle =
+        ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+      const randomX =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+      const randomY =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+      const randomZ =
+        Math.pow(Math.random(), parameters.randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+
+      positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+      positions[i3 + 1] = randomY;
+      positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
+
+      // color
+      const mixedColor = colorInside.clone();
+      mixedColor.lerp(colorOutside, radius / parameters.radius);
+
+      colors[i3 + 0] = mixedColor.r;
+      colors[i3 + 1] = mixedColor.g;
+      colors[i3 + 2] = mixedColor.b;
+    }
+    geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+      size: parameters.size,
+      sizeAttenuation: true,
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+      vertexColors: true,
+    });
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material);
+    scene.add(points);
+  };
+  generateGalaxy();
+
+  useEffect(() => {
+    if (canvas.current !== null) {
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      renderer.setClearColor("#262837");
+
+      renderer.render(scene, camera);
+
+      // Controls
+      const controls = new OrbitControls(
+        camera,
+        canvas.current.appendChild(renderer.domElement)
+      );
+      controls.enableDamping = true;
+      const clock = new THREE.Clock();
+
+      const tick = () => {
+        // Update controls
+        controls.update();
+
+        // galaxy rotation
+        points.rotation.y += 0.001;
+
+        // Render
+        renderer.render(scene, camera);
+
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick);
+      };
+
+      tick();
+    }
+  });
+  return (
+    <CanvasContainer>
+      <div ref={canvas}></div>
+    </CanvasContainer>
+  );
+};
+
+export default Galaxy;
